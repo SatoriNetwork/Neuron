60b33e404fa539a7896c61a5288695f9afd56627
Jordan Miller <Jordan.kay@gmail.com>
2023-12-10 13:29:36 -0700

    debug

--- satorineuron/rendezvous/topic.py
+++ satorineuron/rendezvous/topic.py
@@ -214,12 +214,14 @@ class Topic():
     def getLocalObservation(self, timestamp: str) -> SingleObservation:
         ''' returns the observation before the timestamp '''
         self.data = self.disk.getObservationBefore(timestamp)
+        logging.debug('DATA!!!!', self.data, print='blue')
         if (
             not hasattr(self, 'data') or
-            not hasattr(self, 'hash') or
+            # not hasattr(self, 'hash') or
             self.data is None or
             (isinstance(self.data, pd.DataFrame) and self.data.empty)
         ):
+            logging.debug('ret1', self.data, print='blue')
             return SingleObservation(None, None, None)
         # value, hash are the only columns in the dataframe now
         # if self.streamId.stream in self.data.columns:
@@ -229,13 +231,18 @@ class Topic():
         # else:
         #    column = self.data.columns[0]
         try:
+            row = self.data.loc[self.data.index < timestamp].iloc[-1]
             if (row.shape[0] == 0):
+                logging.debug('ret2', self.data, print='blue')
                 return SingleObservation(None, None, None)
             if (row.shape[0] == 1):
+                logging.debug('ret3', self.data, print='blue')
                 return SingleObservation(row.index, row['value'].values[0], row['hash'].values[0])
-            row = self.data.loc[self.data.index < timestamp].iloc[-1]
+            logging.debug('ret4', self.data, print='blue')
+            # only send 1 row?
             return SingleObservation(row.index, row['value'], row['hash'])
         except IndexError as _:
+            logging.debug('ret5', self.data, print='blue')
             return SingleObservation(None, None, None)
 
     def getLocalCount(self, timestamp: str) -> Union[int, None]:
