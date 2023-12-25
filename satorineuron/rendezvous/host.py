39f73cbef75c78cac2f6e9b1850c33bc15c94007
Jordan Miller <Jordan.kay@gmail.com>
2023-12-24 21:26:33 -0700

    fix

--- satorineuron/rendezvous/host.py
+++ satorineuron/rendezvous/host.py
@@ -135,7 +135,7 @@ class UDPRelay():
                 data, addr = await self.loop.sock_recvfrom(sock, 1024)
                 self.handle(sock, data, addr)
             except Exception as e:
-                greyPrint('listenTo erorr:', e)
+                greyPrint(f'listenTo erorr: {e}')
                 break
         # close?
 
@@ -147,7 +147,7 @@ class UDPRelay():
                 sock.setblocking(False)
                 return sock
             except Exception as e:
-                greyPrint('unable to bind to port', localPort, e)
+                greyPrint(f'unable to bind to port {localPort}, {e}')
             return None
 
         def punch(sock: socket.socket, remoteIp: str, remotePort: int):
@@ -178,7 +178,7 @@ class UDPRelay():
         remotePort: int,
         data: bytes
     ):
-        greyPrint('sending to', remoteIp, remotePort, data)
+        greyPrint(f'sending to {remoteIp}:{remotePort} {data}')
         sock.sendto(data, (remoteIp, remotePort))
 
     async def cancel(self):
