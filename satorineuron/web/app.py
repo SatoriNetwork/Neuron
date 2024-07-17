8e71f53c92e6f8da2f23ea4da1df0ba88aa46353
Jordan Miller <Jordan.kay@gmail.com>
2024-07-17 01:40:19 -0600

    assume other pubsubs

--- satorineuron/web/app.py
+++ satorineuron/web/app.py
@@ -68,7 +68,7 @@ while True:
                 'local': 'http://192.168.0.10:5002',
                 'dev': 'http://localhost:5002',
                 'test': 'https://test.satorinet.io',
-                'prod': 'https://stage2.satorinet.io'}[ENV],
+                'prod': 'https://stage.satorinet.io'}[ENV],
             urlMundo={
                 'local': 'http://192.168.0.10:5002',
                 'dev': 'http://localhost:5002',
@@ -78,7 +78,7 @@ while True:
                 'local': ['ws://192.168.0.10:24603'],
                 'dev': ['ws://localhost:24603'],
                 'test': ['ws://test.satorinet.io:24603'],
-                'prod': ['ws://pubsub2.satorinet.io:24603']}[ENV],
+                'prod': ['ws://pubsub2.satorinet.io:24603','ws://pubsub3.satorinet.io:24603','ws://pubsub4.satorinet.io:24603']}[ENV],
             urlSynergy={
                 'local': 'https://192.168.0.10:24602',
                 'dev': 'https://localhost:24602',
--- satorineuron/web/headless.py
+++ satorineuron/web/headless.py
@@ -77,10 +77,10 @@ while True:
                 'test': 'https://test.satorinet.io',
                 'prod': 'https://mundo.satorinet.io'}[ENV],
             urlPubsubs={
-                'local': ['ws://192.168.0.10:3000'],
-                'dev': ['ws://localhost:3000'],
-                'test': ['ws://test.satorinet.io:3000'],
-                'prod': ['ws://pubsub2.satorinet.io:3000']}[ENV],
+                'local': ['ws://192.168.0.10:24603'],
+                'dev': ['ws://localhost:24603'],
+                'test': ['ws://test.satorinet.io:24603'],
+                'prod': ['ws://pubsub2.satorinet.io:24603', 'ws://pubsub3.satorinet.io:24603', 'ws://pubsub4.satorinet.io:24603']}[ENV],
             urlSynergy={
                 'local': 'https://192.168.0.10:24602',
                 'dev': 'https://localhost:24602',
