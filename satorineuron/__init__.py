5ab5c09cf19262f39eb6e6c7d6ea219a8c4a2be8
Jordan Miller <Jordan.kay@gmail.com>
2024-07-16 18:40:11 -0600

    tested

--- satorineuron/__init__.py
+++ satorineuron/__init__.py
@@ -7,5 +7,5 @@ from satorilib.api.disk import Cache  # Disk
 Cache.setConfig(config)
 logging.setup()
 
-VERSION = '0.1.10'
+VERSION = '0.1.11'
 MOTO = 'Let your workings remain a mystery, just show people the results.'
