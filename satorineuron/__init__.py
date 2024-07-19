3be2d718a729198d9b8f3e0c31c0c1896d702047
Jordan Miller <Jordan.kay@gmail.com>
2024-07-18 22:48:14 -0600

    prediction sampling set to 1 hour

--- satorineuron/__init__.py
+++ satorineuron/__init__.py
@@ -7,5 +7,5 @@ from satorilib.api.disk import Cache  # Disk
 Cache.setConfig(config)
 logging.setup(level=logging.WARNING)
 
-VERSION = '0.1.20'
+VERSION = '0.1.21'
 MOTO = 'Let your workings remain a mystery, just show people the results.'
