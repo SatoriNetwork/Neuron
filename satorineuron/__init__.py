64528089c3678986ec8edc74eb82a448cb61b90f
Jordan Miller <Jordan.kay@gmail.com>
2024-07-22 23:21:19 -0600

    tally update

--- satorineuron/__init__.py
+++ satorineuron/__init__.py
@@ -13,5 +13,5 @@ logging.setup(level={
     'critical': logging.CRITICAL,
 }[config.get().get('logging level', 'warning').lower()])
 
-VERSION = '0.1.22'
+VERSION = '0.1.23'
 MOTO = 'Let your workings remain a mystery, just show people the results.'
