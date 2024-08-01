e58e0e682c31a69ee96185b76e841ba09a5bdb4e
Jordan Miller <Jordan.kay@gmail.com>
2024-08-01 14:44:08 -0600

    added timepout

--- satorineuron/web/app.py
+++ satorineuron/web/app.py
@@ -260,7 +260,7 @@ def passphrase():
             timeout = 1
             return redirect(target)
         else:
-            timeout = timeout * 1.618
+            timeout = min(timeout * 1.618, 60*5)
             return "Wrong passphrase, try again.\n\nIf you're unable to unlock your Neuron remove the setting in the config file."
     next_url = request.args.get('next')
     return render_template_string(passphrase_html, next=next_url)
