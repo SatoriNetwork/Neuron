3dcd741c8edcc1b878bc5ef6f7fd065b8b750b62
Jordan Miller <Jordan.kay@gmail.com>
2024-09-10 23:00:56 -0600

    initial idea

---
+++ WebRTC/signaling_server.py
@@ -0,0 +1,22 @@
+# signaling_server.py
+import asyncio
+import websockets
+
+connected = set()
+
+
+async def signaling(websocket, path):
+    connected.add(websocket)
+    try:
+        async for message in websocket:
+            # Broadcast the received message to all connected peers
+            for conn in connected:
+                if conn != websocket:
+                    await conn.send(message)
+    finally:
+        connected.remove(websocket)
+
+start_server = websockets.serve(signaling, "0.0.0.0", 8765)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
