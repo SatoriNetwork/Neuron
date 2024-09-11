8df01ca27396afe0959a9a9488d72be2ea6393e9
Jordan Miller <Jordan.kay@gmail.com>
2024-09-10 23:01:25 -0600

    update

--- WebRTC/config.py
+++
@@ -1,8 +0,0 @@
-from aiortc import RTCPeerConnection
-
-pc = RTCPeerConnection()
-
-# Adding multiple STUN servers for redundancy
-pc.addIceCandidate({
-    'urls': ['stun:stun.l.google.com:19302']
-})
