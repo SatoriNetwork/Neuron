b25d05f4caa59dd4d9d3763084d25323b05d2303
Mathew-Steeve <mthwsteeve@gmail.com>
2024-09-11 12:29:59 +0530

    peer update

--- WebRTC/peer.py
+++ WebRTC/peer.py
@@ -55,7 +55,7 @@ async def send_offer(websocket):
     # Add STUN server (Google's public one)
     # RTCIceCandidate() 
 
-    pc.addIceCandidate({
+    await pc.addIceCandidate({
         'urls': ['stun:stun.l.google.com:19302']
     })
 
