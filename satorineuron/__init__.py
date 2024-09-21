00d8c25f587cd3654fd1e5ff53bf0fbb20689a80
Jordan Miller <Jordan.kay@gmail.com>
2024-09-20 22:33:39 -0600

    version

--- satorineuron/__init__.py
+++ satorineuron/__init__.py
@@ -13,5 +13,5 @@ logging.setup(level={
     'critical': logging.CRITICAL,
 }[config.get().get('logging level', 'warning').lower()])
 
-VERSION = '0.2.07'
+VERSION = '0.2.08'
 MOTTO = 'Let your workings remain a mystery, just show people the results.'
