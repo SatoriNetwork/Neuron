76941b2f4ce523026523125e48bc0c202980b547
Jordan Miller <Jordan.kay@gmail.com>
2024-09-25 12:30:54 -0600

    Update satori.py

--- satorineuron/web/satori.py
+++ satorineuron/web/satori.py
@@ -609,7 +609,7 @@ def sendSatoriTransactionFromWallet(network: str = 'main'):
     # return sendSatoriTransactionUsing(start.getWallet(network=network), network, 'wallet')
     result = sendSatoriTransactionUsing(
         start.getWallet(network=network), network, 'wallet')
-    if len(result) == 64:
+    if isinstance(result, str) and len(result) == 64:
         flash(str(result))
     return redirect(f'/wallet/{network}')
 
