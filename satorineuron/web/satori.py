0bd0354927c248629c328d6ebaaf5ba008c1b993
Jordan Miller <Jordan.kay@gmail.com>
2024-10-04 14:07:55 -0600

    updated

--- satorineuron/web/satori.py
+++ satorineuron/web/satori.py
@@ -92,7 +92,7 @@ while True:
                 'local': 'http://192.168.0.10:5002',
                 'dev': 'http://localhost:5002',
                 'test': 'https://test.satorinet.io',
-                'prod': 'https://central.satorinet.io'}[ENV],
+                'prod': 'https://stage.satorinet.io'}[ENV],
             urlMundo={
                 'local': 'http://192.168.0.10:5002',
                 'dev': 'http://localhost:5002',
