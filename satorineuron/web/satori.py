6bf588dcfdd2f277001b26265ff03d4866ba4734
Jordan Miller <Jordan.kay@gmail.com>
2024-10-18 09:01:02 -0600

    idk

--- satorineuron/web/satori.py
+++ satorineuron/web/satori.py
@@ -94,9 +94,10 @@ while True:
                 'local': 'http://192.168.0.10:5002',
                 'dev': 'http://localhost:5002',
                 'test': 'https://test.satorinet.io',
+                'prod': 'https://stage.satorinet.io'}[ENV],
                 # 'prod': 'https://central.satorinet.io'}[ENV],
-                # 'prod': 'http://24.199.113.168'}[ENV],
-                'prod': 'http://137.184.38.160'}[ENV],
+                # 'prod': 'http://24.199.113.168'}[ENV], # c
+                # 'prod': 'http://137.184.38.160'}[ENV], # n
             urlMundo={
                 'local': 'http://192.168.0.10:5002',
                 'dev': 'http://localhost:5002',
