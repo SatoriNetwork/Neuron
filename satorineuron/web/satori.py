90db197b0ea2404fc41468af3bf494dbf02729f4
yashshah <yashshah9621@gmail.com>
2024-10-23 13:42:08 +0530

    Add chanegs for local search

--- satorineuron/web/satori.py
+++ satorineuron/web/satori.py
@@ -1859,8 +1859,6 @@ def vote():
 def streams():
     def sanitize_for_json(data):
         import math
-        from flask import jsonify, request, render_template
-
         """
         This function will recursively check the structure and replace any NaN or None
         values with appropriate JSON-compatible values (e.g., None -> null, NaN -> 0).
@@ -1876,64 +1874,31 @@ def streams():
         else:
             return data
         
-    def getStreams(wallet, searchText=None):
-        # todo convert result to the strucutre the template expects:
-        # [ {'cols': 'value'}]
-        streams = start.server.getSanctionVote(wallet, start.vault)
-        if searchText:
-            searchedStreams = [s for s in streams if searchText.lower() in s['stream'].lower()]
-            return sanitize_for_json(searchedStreams)
-        # logging.debug('streams', [
-        #              s for s in streams if s['oracle_alias'] is not None], color='yellow')
+    def getStreams(searchText=None):
+        streams = start.server.getSearchStreams()
+        # Commenting down as of now, will be used in future if we need to make the call to server for search streams
+        # as of now we have limited streams so we can search in client side
+        # if searchText:
+        #     searchedStreams = [s for s in streams if searchText.lower() in s['stream'].lower()]
+        #     return sanitize_for_json(searchedStreams)
         return sanitize_for_json(streams)
-        # return [{
-        #    'sanctioned': 10,
-        #    'active': True,
-        #    'oracle_pubkey': 'pubkey',
-        #    'oacle_alias': 'alias',
-        #    'stream': 'stream',
-        #    'target': 'target',
-        #    'start': 'start',
-        #    'cadence': 60*10,
-        #    'id': '0',
-        #    'total_vote': 27,
-        # }, {
-        #    'sanctioned': 0,
-        #    'active': False,
-        #    'oracle': 'pubkey',
-        #    'alias': 'alias',
-        #    'stream': 'stream',
-        #    'target': 'target',
-        #    'start': 'start',
-        #    'cadence': 60*15,
-        #    'id': '1',
-        #    'vote': 36,
-        # }]
 
-    def accept_submittion(passwordForm):
-        _vault = start.openVault(password=passwordForm.password.data)
-        # if rvn is None and not rvn.isEncrypted:
-        #    flash('unable to open vault')
-
-    if request.method == 'POST':
-        accept_submittion(forms.VaultPassword(formdata=request.form))
-
-    myWallet = start.getWallet(network=start.network)
-    searchText = request.args.get('search', None)  # Get search text from query parameters
-        
-    if searchText is not None:  # Check if the request is a JSON request
-        streamsData = getStreams(myWallet, searchText)
-        return jsonify({'streams': streamsData})  # Return the streams data in a 'streams' key
+    # Commenting down as of now, will be used in future if we need to make the call to server for search streams
+    # as of now we have limited streams so we can search in client side
+    # Get search text from query parameters
+    # searchText = request.args.get('search', None)  
+    # if searchText is not None:
+    #     streamsData = getStreams(searchText)
+    #     return jsonify({'streams': streamsData})
 
     return render_template('streams.html', **getResp({
         'title': 'Streams',
         'network': start.network,
         'vaultPasswordForm': presentVaultPasswordForm(),
         'vaultOpened': False,
-        'wallet': myWallet,
         'vault': start.vault,
         'darkmode': darkmode,
-        'streams': getStreams(myWallet, searchText)}))
+        'streams': getStreams()}))
 
 
 @app.route('/proposals', methods=['GET'])
