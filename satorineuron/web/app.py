df889bb126b8bbc4efd07bbb473f6877983c2cce
Jordan Miller <Jordan.kay@gmail.com>
2024-10-27 10:37:38 -0600

    pull latest on startup

--- satorineuron/web/app.py
+++ satorineuron/web/app.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import time
 import subprocess
@@ -7,24 +8,37 @@ def startSatori():
     return subprocess.Popen([sys.executable, 'satori.py'])
 
 
+def pullSatori():
+    process = subprocess.Popen(['/bin/bash', 'pull.sh'])
+    process.wait()
+
+
+def isDevMode() -> bool:
+    return os.environ.get('ENV', os.environ.get('SATORI_RUN_MODE', 'dev')) != 'prod'
+
+
 def monitorAndRestartSatori():
     while True:
         print("Starting Satori...")
+        # actually it seems we can interrupt with ctrl+c either way
+        # isDev= isDevMode()
+        pullSatori()
         process = startSatori()
-        while True:
-            try:
-                print(f'pol')
-                return_code = process.poll()
-                print(f'pol2')
-                if return_code is not None:
-                    print(f'Satori exited with code {return_code}.')
-                    break
-                time.sleep(1)
-            except KeyboardInterrupt:
-                print("Shutting down monitor...")
-                process.terminate()
-                process.wait()
-                return
+        # if not isDev:
+        process.wait()
+        # else: #(must be able to interrupt with ctrl+c)
+        #    while True:
+        #        try:
+        #            return_code = process.poll()
+        #            if return_code is not None:
+        #                print(f'Satori exited with code {return_code}.')
+        #                break
+        #            time.sleep(1)
+        #        except KeyboardInterrupt:
+        #            print("Shutting down monitor...")
+        #            process.terminate()
+        #            process.wait()
+        #            return
 
 
 if __name__ == "__main__":
