c98ec68d25408a62c34e97b44b7fd703d575de1e
Jordan Miller <Jordan.kay@gmail.com>
2024-12-19 16:08:40 -0700

    loading environment variables

--- satorineuron/web/app.py
+++ satorineuron/web/app.py
@@ -5,6 +5,23 @@ import subprocess
 
 lastPull = 0
 
+def loadEnvVars(envFile):
+    """
+    Load environment variables from a file into os.environ.
+    """
+    if os.path.exists(envFile):
+        with open(envFile) as f:
+            for line in f:
+                # Skip comments and empty lines
+                line = line.strip()
+                if line and not line.startswith('#'):
+                    key, _, value = line.partition('=')
+                    os.environ[key.strip()] = value.strip()
+        #print(f"Loaded environment variables from {envFile}")
+    else:
+        #print(f"Environment file {envFile} not found. Skipping...")
+        pass
+
 
 def startSatori():
     return subprocess.Popen([sys.executable, '/Satori/Neuron/satorineuron/web/satori.py'])
@@ -67,5 +84,6 @@ def monitorAndRestartSatori():
 
 
 if __name__ == "__main__":
+    loadEnvVars('/Satori/Neuron/config/vars.env')
     return_code = monitorAndRestartSatori()
     os._exit(return_code)
