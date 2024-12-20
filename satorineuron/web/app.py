e0115e724ef48535f52703531dba39b702e810b7
Jordan Miller <Jordan.kay@gmail.com>
2024-12-19 21:32:29 -0700

    deployed new update system

--- satorineuron/web/app.py
+++ satorineuron/web/app.py
@@ -2,8 +2,7 @@ import os
 import sys
 import time
 import subprocess
-
-lastPull = 0
+from satorineuron.update import update
 
 def loadEnvVars(envFile):
     """
@@ -22,50 +21,22 @@ def loadEnvVars(envFile):
         #print(f"Environment file {envFile} not found. Skipping...")
         pass
 
-
 def startSatori():
     return subprocess.Popen([sys.executable, '/Satori/Neuron/satorineuron/web/satori.py'])
-
-
-def pullSatori():
-    process = subprocess.Popen(
-        ['/bin/bash', 'pull.sh'],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE)
-    #process.wait()
-    stdout, stderr = process.communicate()
-    #print("STDOUT:", stdout.decode())
-    #print("STDERR:", stderr.decode())
-    global lastPull
-    lastPull = time.time()
 
 
 def isProdMode() -> bool:
     return os.environ.get('ENV', os.environ.get('SATORI_RUN_MODE', 'dev')) == 'prod'
 
-
-def allowedToPull() -> bool:
-    '''
-    open /Satori/Neuron/config/config.yaml if it exists,
-    check if the value of 'pull code updates' is true
-    '''
-    config_path = '/Satori/Neuron/config/config.yaml'
-    if os.path.exists(config_path):
-        with open(config_path, 'r') as f:
-            lines = f.readlines()
-            for line in lines:
-                if line.startswith('pull code updates'):
-                    return line.split(':')[1].strip().lower() == 'true'
-    return True
+def isNotWalletMode() -> bool:
+    return os.environ.get('RUNMODE') != 'wallet'
 
 
 def monitorAndRestartSatori():
     while True:
-        print("Starting Satori...")
-        if allowedToPull() and time.time() - lastPull > 60*60:
-            pullSatori()
-        else:
-            print("skipped pull...")
+        if isProdMode() and isNotWalletMode():
+            update()
+        print('Starting Satori...')
         process = startSatori()
         while True:
             try:
@@ -77,13 +48,13 @@ def monitorAndRestartSatori():
                     return return_code  # 0 shutdown, 1 restart container, err
                 time.sleep(1)
             except KeyboardInterrupt:
-                print("Shutting down monitor...")
+                print('Shutting down monitor...')
                 process.terminate()
                 process.wait()
                 return 0
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     loadEnvVars('/Satori/Neuron/config/vars.env')
     return_code = monitorAndRestartSatori()
     os._exit(return_code)
