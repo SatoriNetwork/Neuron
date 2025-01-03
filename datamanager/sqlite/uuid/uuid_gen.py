c537ff6ecc7f4aa651d99295aa88574742e60ebf
krizhnaa <krizhnaprem@gmail.com>
2025-01-03 21:04:34 +0530

    UUID generator takes all stream identifiers

--- datamanager/sqlite/uuid/uuid_gen.py
+++ datamanager/sqlite/uuid/uuid_gen.py
@@ -1,12 +1,19 @@
 import uuid
 
-def generate_uuid(author: str, stream: str) -> uuid:
+
+def generate_uuid(data: dict) -> uuid:
     namespace = uuid.NAMESPACE_DNS
-    combined = f"{author}:{stream}"
+    combined = ':'.join(str(v) for v in data.values())
     return uuid.uuid5(namespace, combined)
 
+
 # Usage
-# author = "03616262b0241e62f2971b59bb4685485a632c16cc6acae34449910781fd7057c2"
-# stream = "ICE.USD.10mins_p"
-# result = generate_uuid(author, stream)
+# stream_identifier = {
+#     "source": "OpenWeatherMap API",
+#     "author": "034f57b8421b40575149d2be03d4d27bfc7e345b5ef8f478995ae2970f9a3e4e9d",
+#     "stream": "Tokyo_pressure",
+#     "target": "result.pressure",
+# }
+# result = generate_uuid(stream_identifier)
 # print(result)
+# '3f6d8b21-eb66-57c5-8315-3094c0fb7739'
