1d668f53be241bb8417445df7fe5c00447cf56f8
krizhnaa <krizhnaprem@gmail.com>
2025-01-12 12:16:44 +0530

    p2p roadmap

--- datamanager/datastreamservice/server.py
+++ datamanager/datastreamservice/server.py
@@ -1,3 +1,26 @@
+'''
+
+refactor and test
+---
+create a server - rendezvous server
+--- 
+integrate with neuron-dataClient
+integrate with engine-dataClient
+---
+subsume all logic currently used in engine for managing data on disk
+---
+make sure it's all working together
+---
+p2p - get histories (just request history of data)
+p2p - replace pubsub servers (start using subscriptions)
+---
+relay if necessary?
+---
+
+
+
+'''
+
 import asyncio
 import websockets
 import json
