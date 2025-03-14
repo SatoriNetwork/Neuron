6fd9367d4c6dd7d17a87e2cf17d1c25b54680564
Jordan Miller <Jordan.kay@gmail.com>
2025-03-14 13:52:20 -0600

    fix

--- satorineuron/web/start.sh
+++ satorineuron/web/start.sh
@@ -12,9 +12,8 @@ if [ "$HEADLESS" = "True" ]; then
     python headless.py
 else
     # Start required background processes
-    nohup python /Satori/Neuron/satorinueron/web/data.py > data.log 2>&1 &
+    nohup python /Satori/Neuron/satorineuron/web/data.py > data.log 2>&1 &
     nohup python /Satori/Engine/satoriengine/veda/enginerun.py > enginerun.log 2>&1 &
-    nohup python /Satori/Lib/satorilib/datamanager/server.py > server.log 2>&1 &
 
     # Run main application
     python app.py
