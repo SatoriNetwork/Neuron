e7e05a6bfae1b9c807d4a85c16eb3d9da67e763d
Jordan Miller <Jordan.kay@gmail.com>
2025-03-14 09:51:38 -0600

    run with debugging

--- satorineuron/web/start.sh
+++ satorineuron/web/start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-#ipfs init;
-#ipfs daemon &
+
+# Uncomment if IPFS is required
+# ipfs init
+# ipfs daemon &
 
 # Check if config.yaml has headless set to True
 HEADLESS=$(python headless_check.py)
@@ -9,6 +11,11 @@ if [ "$HEADLESS" = "True" ]; then
     echo "Running in headless mode"
     python headless.py
 else
-    # python data.py # run DataManager app in the background
+    # Start required background processes
+    nohup python /Satori/Neuron/satorinueron/web/data.py > data.log 2>&1 &
+    nohup python /Satori/Engine/satoriengine/veda/enginerun.py > enginerun.log 2>&1 &
+    nohup python /Satori/Lib/satorilib/datamanager/server.py > server.log 2>&1 &
+
+    # Run main application
     python app.py
 fi
