c8ad47b1644b1dad7315393581fe0516cb9f04b9
krizhnaa <krizhnaprem@gmail.com>
2025-09-23 22:11:32 +0400

    version change

--- satorineuron/__init__.py
+++ satorineuron/__init__.py
@@ -10,5 +10,5 @@ logging.setup(level={
     'critical': logging.CRITICAL,
 }[config.get().get('logging level', 'info').lower()])
 
-VERSION = '0.4.22'
+VERSION = '0.4.23'
 MOTTO = 'Let your workings remain a mystery, just show people the results.'
